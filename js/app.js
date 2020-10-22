a87714099725042e3376765fae5acf06d911a68a
shllshckd <52105864+shllshckd@users.noreply.github.com>
2020-10-22 11:21:51 +0300

    add separate page for every component, add more navigation routes
    add separate page for every component, add more navigation routes

--- js/app.js
+++ js/app.js
@@ -8,13 +8,17 @@ document.addEventListener('DOMContentLoaded', function () {
     var instances = M.Materialbox.init(elems);
 });
 
-
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems);
 });
 
 var instance = M.Carousel.init({
-    fullWidth: true, 
+    fullWidth: true,
     indicators: true
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems);
 });
